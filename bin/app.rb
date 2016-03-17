e8c64f4076e4277c0c8a7c575a786cc8a10dacc4
Scott Leitch <s.d.leitch@gmail.com>
2016-03-17 16:21:10 -0400

    add tag erb, basic tagging seems to work. had conneciton_pool issue

--- bin/app.rb
+++ bin/app.rb
@@ -6,9 +6,26 @@ set :public_folder, "static"
 set :views, "views"
 
 class Document < ActiveRecord::Base
+  def tag_document(params)
+    self.interesting += 1 if params['interesting'] == 'on'
+    self.funny += 1 if params['funny'] == 'on'
+    self.boring += 1 if params['boring'] == 'on'
+    self.save!
+  end
 end
 
 get '/' do
   @documents = Document.all
   erb :index
+end
+
+get '/tag/' do
+  @document = Document.all.sample
+  erb :tag
+end
+
+post '/tag/' do
+  @document = Document.find(params['id'])
+  @document.tag_document(params)
+  redirect to('/')
 end
