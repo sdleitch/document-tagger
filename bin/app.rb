db4266a428589495e67915fe15351ad20c919d21
Scott Leitch <s.d.leitch@gmail.com>
2016-03-17 19:34:11 -0400

    basic sessions work. need to add secret

--- bin/app.rb
+++ bin/app.rb
@@ -6,23 +6,27 @@ require'./lib/models/document.rb'
 set :public_folder, "static"
 set :views, "views"
 
+enable :sessions
+
 get '/' do
+  session[:tagged_docs] = [] if session[:tagged_docs].nil?
   @documents = Document.all
   erb :index
 end
 
-get '/tag' do
+get '/tag/' do
   @document = Document.all.sample
   erb :tag
 end
 
-post '/tag' do
+post '/tag/' do
   @document = Document.find(params['id'])
   @document.tag_document(params)
+  session[:tagged_docs] << @document.id
   redirect to '/'
 end
 
-get '/:id' do
+get '/:id/' do
   @document = Document.find(params['id'])
   erb :view_document
 end
